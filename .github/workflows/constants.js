1a84c5c3d5425f2ec3db31669d293e53e6cc539b
Tanmay Gupta <tanmay8.mitmpl2022@learner.manipal.edu>
2024-07-14 15:54:44 +0530

    Add files via upload

---
+++ .github/workflows/constants.js
@@ -0,0 +1,65 @@
+export const scaleFactor = 1.3;
+// export const PlayerscaleFactor = 4;
+
+export const dialogueData = {
+    "Summary": "Hey! I just finished my second year in Computer and Communication Engineering at Manipal Institute of Technology (MIT). This journey has really helped me build a strong foundation and confidence in Data Structures, as well as sharpen my critical thinking and problem-solving skills.",
+    "Projects":"I've built some really cool projects, like a personal voice assistant called Jarvis, a property rental website named LeaseLink, and an algorithm visualizer that showcases sorting and graph traversal algorithms. For more interesting projects, check out my <a href=https://github.com/TanmayGupta17>GitHub</a>!",
+    "Education":"I completed my 10th and 12th grades under the Central Board of Secondary Education (CBSE).which provided a strong educational framework and academic rigor. These formative years were instrumental in nurturing my interest in computer science and engineering. Currently, I am pursuing a degree in Computer and Communication Engineering at the Manipal Institute of Technology (MIT). Here, I am continuously expanding my knowledge and honing my skills in this dynamic and ever-evolving field.",
+    "Skills":"Over the years, I’ve honed my skills in various programming languages such as C, Java, C#, Python, MySQL, HTML, CSS, and JavaScript. Alongside these, I’ve become proficient in using tools like Git, GitHub, VSCode, and Visual Studio, which have greatly enhanced my coding and development workflow.",
+    "ContactMe": `<h3>Contact Me</h3>
+    <div style="display: flex; margin-right: 10px;">
+        <div class="Email" style="border: 2px solid #333; padding: 5px 10px; border-radius: 10px; width: 125px; background-color: #e30d0d; color: white; height: 40px;">
+            <p style="display: flex; align-items: center; color: white; height: 100%; margin: 0;">
+                <img src="./gmailLogo.jpg" alt="Email" style="width: 25px; height: 20px; margin-right: 10px;" />
+                <a href="mailto:TanmayGupta1706@gmail.com" style="color: white; text-decoration: none;">Email</a>
+            </p>
+        </div>
+        <div class="Github" style="border: 2px solid #333; padding: 5px 10px; border-radius: 10px; width: 125px; background-color: black; margin-left: 20px; height: 40px;">
+            <p style="display: flex; align-items: center; height: 100%; margin: 0;">
+                <img src="./githubLogo.jpg" alt="GitHub" style="width: 25px; height: 20px; margin-right: 10px;" />
+                <a href="https://github.com/TanmayGupta17" target="_blank" style="color: white; text-decoration: none;">GitHub</a>
+            </p>
+        </div>
+        <div class="Linkdin" style="border: 2px solid #333; padding: 5px 10px; border-radius: 10px; width: 125px; background-color: #0166c8; margin-left: 20px; height: 40px;">
+            <p style="display: flex; align-items: center; height: 100%; margin: 0;">
+                <img src="./linkdinlogo.avif" alt="GitHub" style="width: 25px; height: 20px; margin-right: 10px;" />
+                <a href="https://www.linkedin.com/in/tanmay-gupta-link/" target="_blank" style="color: white; text-decoration: none;">Linkedin</a>
+            </p>
+        </div>
+    </div>`
+    
+
+    //education: i did my 10 and 12th from central board of secondary education
+    // <span style="margin-left: 5px;">↗</span>
+
+
+    // "ContactMe":`For more information or to get in touch with me:
+    // <br>
+    // ➤ Email: <a href='mailto:your-email@gmail.com'>TanmayGupta1706@gmail.com</a>
+    // <br>
+    // ➤ GitHub: <a href='https://github.com/TanmayGupta17' target='_blank'>https://github.com/TanmayGupta17</a>`
+
+
+
+
+
+    // pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    //   I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    //   Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+    // "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    // "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+    // I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
+    // You'll learn how I built this portfolio you're currently playing through!`,
+    // tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+    //  <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+    // <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+    // bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    // resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+    // Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+    // projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    // Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+    // Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    // library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+    // I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    // exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  };
