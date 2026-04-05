05a447c981b13b9fff85e238aef6eeb79c6e3902
tanmaygupta17 <tanmaygupta1706@gmail.com>
2026-04-05 18:30:23 +0530

    Fix GitHub Pages build base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
 import { defineConfig } from "vite";
 
-const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
-
 export default defineConfig({
-  base: isGitHubActions ? "/Portfolio1/" : "/",
+  base: "/Portfolio1/",
   server: {
     open: true, // Automatically open the browser when the server starts
   },
